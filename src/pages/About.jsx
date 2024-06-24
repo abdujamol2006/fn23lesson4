62fcb4f471609665ee7ec84c052c1073f0f83a55
Abdujamol <hamdamjonovabdujamol@gmail.com>
2024-06-24 16:47:38 +0500

    aaa

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 function About() {
-  return <div></div>;
+  return (
+    <div className="site-container ">
+      <h1 className="about-text text-5xl font-bold">About</h1>
+      <p className="text-2xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+        illo quo dolorum quas soluta aperiam voluptate? Rem incidunt quas
+        voluptate quidem dolor fugiat. Id, aut laudantium non, illo soluta
+        repellendus perspiciatis tempore accusantium culpa consequuntur impedit
+        ducimus eius exercitationem dolore asperiores similique voluptates
+        voluptate placeat deserunt nesciunt saepe! Asperiores itaque accusamus
+        odit et veniam fugit dolores quas. Qui quisquam dolorum libero nisi
+        optio alias eos vitae eveniet, inventore sequi quam, fugit distinctio
+        tempore iusto maiores dolores earum. Corporis qui quas deleniti iure
+        libero blanditiis. Ab obcaecati eos vitae accusamus a autem totam
+        impedit, voluptates, voluptatem, id voluptatibus explicabo beatae alias.
+      </p>
+    </div>
+  );
 }
 
 export default About;
