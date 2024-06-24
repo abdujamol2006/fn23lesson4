fa1141dc3a7fbdb5bcdb0090de9f9f5e3d467768
Abdujamol <hamdamjonovabdujamol@gmail.com>
2024-06-24 15:46:46 +0500

    err

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useRouteError } from "react-router-dom";
+import { RiCriminalLine } from "react-icons/ri";
+import { GiGroundbreaker } from "react-icons/gi";
 
 function ErrorPage() {
   const error = useRouteError();
   if (error.status == 404) {
     return (
-      <div className="black ">
-        <div className="  errorpage ">
-          <h1 className="errorpage-text ">Page not founded :(</h1>
+      <div className=" bg-accent-content ">
+        <div className="  errorpage min-h-screen flex justify-center items-center ">
+          <div className="num flex">
+            <h2 className=" font-bold text-9xl mt">404</h2>
+            <RiCriminalLine className="w-10 h-10" />
+          </div>
+          <h1 className="errorpage-text flex gap-5 text-6xl font-bold ">
+            Page not founded <GiGroundbreaker />
+          </h1>
           <p className="err">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga,
             delectus blanditiis iure vero dicta nobis expedita tempora aliquid
